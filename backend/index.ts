e01bfa0117151b128810a9da3fa60e1aff973101
panch8 <98569130+panch8@users.noreply.github.com>
2023-09-04 12:44:12 +0200

    first-commit

---
+++ backend/index.ts
@@ -0,0 +1,270 @@
+import { $query, $update, nat,Principal,Record, StableBTreeMap, text, Vec, ic, Opt, Variant, match, Tuple, Result } from 'azle';
+
+
+let votingSupply : nat = BigInt(0);
+let startTimes: Vec<Tuple<Times>> = [];
+let expTimes: Vec<Tuple<Times>> = [];
+
+//Type Times <tuple> {timestamp, id}
+type Times = Tuple<[nat,nat]>
+
+type User= Record<{
+    id: Principal,
+    userName: text,
+    metadata: text,
+    votingPower: nat,
+    votedProposalsId: Vec<nat>,
+    proposals: Vec<nat>
+}>
+type Status = Variant<{
+    pending: null,
+    onVoting: null,
+    finish: null,
+    accepted:null,
+    rejected: null        
+}>;
+
+type Proposal = Record<{
+    id: nat,
+    issuer: Principal,
+    iat: nat,
+    start:nat,
+    exp: nat,
+    title: text,
+    description: text,
+    status: Status,
+    voteYes: nat,
+    voteNo: nat,
+    participation: nat
+}>
+
+let userDb = new StableBTreeMap<Principal, User>(0,50,1_000);
+let proposalsDb = new StableBTreeMap<nat, Proposal>(1,20,10_000)
+
+
+//Users logic
+$update;
+export function setUser(userName:text): User {
+    
+    const user = {
+        id: ic.caller(),
+        userName: userName,
+        metadata: '',
+        votingPower: BigInt(1),
+        votedProposalsId: [],
+        proposals: []
+    }
+    userDb.insert(ic.caller(), user)
+    inc();
+    return user;
+}
+
+
+$query;
+export function seeUsers(): Vec<User>{
+    return userDb.values()
+}
+
+
+$query;
+export function seeMyUser(): Opt<User>{
+    return userDb.get(ic.caller())
+}
+
+$query;
+export function seeOneUser(id: text): Opt<User>{
+    const selectedUser = userDb.get(Principal.fromText(id));
+    return selectedUser;
+}
+
+$update;
+export function updateMetaObj(metaDataObj : text): boolean {
+    const userToBeUpdated = userDb.get(ic.caller());
+  
+    if(userToBeUpdated.Some !== undefined){
+        userToBeUpdated.Some.metadata = metaDataObj;
+        userDb.insert(ic.caller(),userToBeUpdated.Some)
+        console.log('user updated');
+        return true;
+    } else {
+        console.log('something went wrong');
+        return false;
+    }
+
+}
+
+//manca
+$update;
+export function deleteMyUser():boolean{
+    return match(userDb.remove(ic.caller()),{
+        Some: ()=> {
+            dec();
+            return true},
+        None: ()=> false
+    })
+
+}
+
+
+
+
+
+///////////////////// PROPOSALS /////////////////////
+
+$query;
+export function seeProposals ():Vec<Proposal>{
+    const proposals = proposalsDb.values()
+    return proposals;
+}
+
+$query;
+export function getProposalById(id:nat): Opt<Proposal>{
+    return proposalsDb.get(id);
+}
+
+
+//check maybe re-deploy
+$query;
+export function getProposalByStatus(status: Status): Opt<Vec<Proposal>>{
+        const propsArr = Array.from(proposalsDb.values());
+        console.log(propsArr,'arrr',proposalsDb.values());
+        const propsByStatus = propsArr.filter((item)=>{
+            console.log(item.status,"lets see", status);
+            item.status === status;
+        })
+        if(propsByStatus.length === 0){ return Opt.None}
+   return Opt.Some(propsByStatus)
+};
+    
+$update;
+export function submitProposal(title: text, description: text, gotVoting: nat, expiration: nat): Proposal {
+    //time in seconds
+    const newProposal: Proposal = {
+        id: proposalsDb.len() + BigInt(1),
+        issuer: ic.caller(),
+        iat: ic.time()/BigInt(1000)/BigInt(1000)/BigInt(1000),
+        start: gotVoting,
+        exp: expiration,
+        title: title,
+        description: description,
+        status: { pending: null},
+        voteYes: 0n,
+        voteNo: 0n,
+        participation: 0n
+
+
+    }
+    if(newProposal.iat < newProposal.start && newProposal.start < newProposal.exp ){
+        console.log(newProposal);
+    const start_time: Times = [newProposal.start, newProposal.id]
+    const exp_time: Times = [newProposal.exp, newProposal.id]
+    startTimes.push(start_time);
+    expTimes.push(exp_time);
+    proposalsDb.insert(newProposal.id, newProposal)
+    console.log("STRTSS",startTimes,"EXPSSS",expTimes);
+    return newProposal
+    }else{
+        console.log(`iat: ${newProposal.iat}, start time: ${newProposal.start}, or expiration: ${newProposal.exp} time not correspond`);
+
+        newProposal.status = {rejected:null};
+        return newProposal
+    }
+    
+
+
+}
+
+$update;
+export function changeStatus(id:nat): void {
+    match(proposalsDb.get(id), {
+        Some: (proposal) => {
+            console.log("prooposal", proposal.status);
+         switch (proposal.status) {
+            case {pending:null}:
+                if(ic.time()/BigInt(1000)/BigInt(1000)/BigInt(1000)> proposal.start){
+                proposal.status = {onVoting:null};
+                startTimes.splice(startTimes.indexOf([proposal.start, proposal.id]),1);
+                };
+            case {onVoting: null}:
+                if(ic.time()/BigInt(1000)/BigInt(1000)/BigInt(1000)> proposal.exp){
+                proposal.status = {finish:null};
+                expTimes.splice(expTimes.indexOf([proposal.exp, proposal.id]),1);
+                };
+                break;
+            case {finish:null}:
+                if(proposal.participation>0){
+                    if(proposal.voteYes>proposal.voteNo){
+                        proposal.status = {accepted:null}
+                    }else proposal.status = {rejected:null} ;
+                }else{
+                    proposal.status = {rejected:null};
+                    proposal.description = `Proposal rejected due to no participation`
+                }
+
+                break;
+            };
+            console.log("proposal Update", proposal);
+            console.log("startTs array", startTimes);
+            console.log("expTs array", expTimes);
+          proposalsDb.insert(id,proposal); 
+        },
+        None: () => { console.log(`Proposal with id: ${id} does not exist`);}
+    })
+   
+}
+
+$update;
+export function voteOnProposal(id:nat, vote:boolean): Result<text, text> {
+    return match(proposalsDb.get(id), {
+        Some: (proposal) => {
+            if(proposal.status.onVoting){
+                if(vote === true){ 
+                    proposal.voteYes += BigInt(1);
+                    proposal.participation += BigInt(1);
+                }
+                else {
+                    proposal.voteNo += BigInt(1);
+                    proposal.participation += BigInt(1);
+                }
+                return { Ok:` Your vote was successfully casted to the proposal: ${id}`}
+            }return { Err: `This proposal is not available for voting. Current Status: ${proposal.status}`}
+        },
+        None: ()=>{
+            return {
+                Err:`No proposal listed with id: ${id}`
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+///////////////////////// VOTING SUPPLY ////////////////////
+$query;
+export function get(): nat {
+    return votingSupply;
+}
+
+$query;
+export function getIcTimeSeconds(): nat {
+    return ic.time()/BigInt(1000)/BigInt(1000)/BigInt(1000);
+}
+//when a new user is registered increment voting supply
+
+function inc(): nat {
+    votingSupply += BigInt(1);
+    return votingSupply
+}
+
+// when user is deleted decrease voting supply
+
+function dec(): nat {
+    votingSupply -= BigInt(1);
+    return votingSupply
+}
+
---
+++ src/App.tsx
@@ -0,0 +1,214 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import azleLogo from './assets/azle_logo.svg';
+import azleShadow from './assets/azle_shadow.png';
+import { backend } from './declarations/backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { Actor, HttpAgent} from '@dfinity/agent';
+import { idlFactory } from './declarations/backend';
+
+
+let authClientStore: AuthClient | null = null;
+
+function App() {
+  const [count, setCount] = useState<number | undefined>();
+  const [loading, setLoading] = useState(false);
+  const [logged, setLogged] = useState(false);
+
+  // Get the current counter value
+  const fetchCount = async () => {
+    try {
+      setLoading(true);
+      const count = await backend.get();
+      setCount(+count.toString()); // Convert BigInt to number
+      if(authClientStore?.isAuthenticated()){
+      setLogged(true);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+const displayMessage = (message: string): void=>{
+  console.log(`${message}`);
+   document.querySelector('.card')?.insertAdjacentHTML('afterbegin',`<h3 id="msg">${message}</h3>`);
+   setTimeout(()=>{
+    document.getElementById('msg')?.remove();
+   },2500)
+}
+
+//  const notLogged = async ()=>{
+//   const isAuth = await authClientStore?.isAuthenticated();
+//   if(!isAuth){
+//    displayMessage('You Must Log in')
+//    return true;
+//   }else return false;
+//  };
+
+ const login = async () => {
+  const authClient = await AuthClient.create();
+  const isLocalNetwork = process.env.DFX_NETWORK == 'local';
+  const identityProviderUrl = isLocalNetwork ? 
+      `http://127.0.0.1:4943/?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}` : 
+      'https://identity.ic0.app/';
+  authClient.login({
+    identityProvider: identityProviderUrl,
+    maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+    onSuccess: async () => {
+      authClientStore = authClient;    
+      setLogged(true);
+    },
+  });
+
+};
+
+const logout = async () =>{
+  await authClientStore?.logout();
+  setLogged(false)
+}
+
+  const updateMetadata = async (event: any) =>{
+    event.preventDefault();
+    if(!logged){
+      displayMessage("You must be an active member logged in");
+      return;
+    }
+    let meta = event.target.closest('form').querySelector('input').value
+    typeof meta !== 'string' && (meta = JSON.stringify(meta))
+    console.log(meta);
+     const result = await backend.updateMetaObj(meta);
+     if(result){
+      displayMessage('Successfully updated metadata')
+     }else {console.log('smth happn ')}
+  }
+
+  const seeAll = async ()=>{
+    const result =  await backend.seeUsers();
+    console.log(result)
+    if(result.length>0 ){
+      document.getElementById('root')?.insertAdjacentHTML('beforeend',`<h3> ${result}</h3>`)
+     }else displayMessage('Not yet registered users');
+
+  }
+
+  const setNewMember = async (event: any)=>{
+    event.preventDefault();
+    if(!logged){
+      displayMessage('You must log in to become a Member =)')
+      return;}
+    const userName =event.target.closest('form').querySelector('input').value;
+
+    const exist = await backend.seeMyUser();
+    console.log(exist);
+    if (exist.length > 0){
+      displayMessage('You are already a registered member')
+      return;}
+    await backend.setUser(userName);
+    displayMessage(`Yey!! Welcome to our community   ${userName}!!!!`);
+  }
+
+const addToCount =async (e: any) => {
+  if(loading)return;
+  e.preventDefault();
+  const sum =BigInt(+ e.target.closest('form').querySelector('input').value); 
+  console.log(sum);
+  try {
+    setLoading(true);
+    await backend.add(sum); // Increment the count by argument
+    await fetchCount(); // Fetch the new count
+    
+  } finally {
+    setLoading(false);
+  }
+
+}
+
+
+  const increment = async () => {// only if authenticated
+    if (loading) return; // Cancel if waiting for a new count
+    if(!logged){
+      displayMessage("You must log in")
+      return;}
+
+    try {
+        setLoading(true);
+        await backend.inc(); // Increment the count by 1
+        await fetchCount(); // Fetch the new count
+        
+      } finally {
+        setLoading(false);
+      }
+
+  };
+
+
+  
+ // Fetch the count on page load
+  useEffect(() => {
+    fetchCount();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className='Nav'>
+      <button id='login-btn' onClick={login} style={{ opacity: loading ? 0.5 : 1, display: logged? "none": "inline-block" }}>
+          Log in here
+        </button>
+      <button  onClick={logout} style={{ opacity: loading ? 0.5 : 1, display: logged ? "inline-block":"none"}}>
+          Log out
+        </button>
+      </div>
+      <div>
+     
+        <a
+          href="https://github.com/demergent-labs/azle"
+          target="_blank"
+        >
+          <span className="logo-stack">
+            <img
+              src={azleShadow}
+              className="logo azle-shadow"
+              alt="azle logo"
+            />
+            <img src={azleLogo} className="logo azle" alt="Azle logo" />
+          </span>
+        </a>
+        
+      </div>
+      <h1> I don't Care D.A.O 🫡 </h1>
+      <h3> Cnverting useless proposals into a community commitment</h3>
+      <div className="card">
+        
+        <button  onClick={increment} style={{ opacity: loading ? 0.5 : 1 }}>
+          Voting Supply {count}
+        </button>
+        <form>
+          <input type='text' placeholder='username'></input>
+          <button type='submit' onClick={setNewMember} style={{ opacity: loading ? 0.5 : 1 }}>
+          Become a Member
+        </button>
+        </form>
+        <form>
+          <input type='text' placeholder='tell something about you'></input>
+          <button type='submit' onClick={updateMetadata} style={{ opacity: loading ? 0.5 : 1 }}>
+          Save data
+        </button>
+        </form>
+        <form>
+          <input type='text' placeholder='add only if authenticated'></input>
+          <button type='submit' onClick={addToCount} style={{ opacity: loading ? 0.5 : 1 }}>
+          ADD
+        </button>
+        </form>
+         <button onClick={seeAll} style={{ opacity: loading ? 0.5 : 1 }}>
+          see all members
+        </button>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
